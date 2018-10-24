ba3b2475a7645e076647dba554cd8348c9a8e59f
Fatih Şen <fatih@fatih-macbook-pro.local>
2018-10-24 16:55:09 +0300

    Initial Commit

---
+++ MVI/MVI.h
@@ -0,0 +1,19 @@
+//
+//  MVI.h
+//  MVI
+//
+//  Created by Fatih Şen on 24.10.2018.
+//  Copyright © 2018 Fatih Şen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVI.
+FOUNDATION_EXPORT double MVIVersionNumber;
+
+//! Project version string for MVI.
+FOUNDATION_EXPORT const unsigned char MVIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVI/PublicHeader.h>
+
+
