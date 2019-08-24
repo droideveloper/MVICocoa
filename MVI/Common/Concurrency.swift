bedf8443ec8bc9707c3a5c89e594e51e1b205ef6
droideveloper <fatih.sen.boun@gmail.com>
2019-08-24 12:11:29 +0300

    added concurrency implementations

---
+++ MVI/Common/Concurrency.swift
@@ -0,0 +1,15 @@
+//
+//  Concurrency.swift
+//  MVI
+//
+//  Created by Fatih Şen on 24.08.2019.
+//  Copyright © 2019 Fatih Şen. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+public protocol Concurrency {
+	
+	var dispatchScheduler: SerialDispatchQueueScheduler { get }
+}
